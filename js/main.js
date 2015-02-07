b6b03c8e4978530e1ffbd9c50d6e77dc5e37b83e
ssstrieu <samanthastrieu@gmail.com>
2015-02-07 15:08:03 -0800

    update io index
    updated index content and linked other pages

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+//debugger
+
+$(document).ready(function(){
+
+	$('.dropdown').mouseenter(function(){
+		console.log('mouseoveredeeded');
+		});
+	$('.dropdown').mouseleave(function(){
+		console.log('mouse left');
+		});
+
+});
+
+
+
+// var getItem=function(){
+// 	$.ajax({
+// 		url:'js/itemLibrary.json',
+// 		type:"GET",
+// 		dataType:"json",
+// 		success: function(response){
+// 			inventory.push(response);
+// 		}
+
+//to obtain an item call getItem(potion);
+
+
+
+	
+
+
